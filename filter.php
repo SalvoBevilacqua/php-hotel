275658ae8fdb34b9c3d162adb5e3690540d8d891
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2023-12-12 19:26:01 +0100

    wip: bonus

--- filter.php
+++ filter.php
@@ -1,66 +1,81 @@
+<?php
+$filter = $_GET['filter'];
+?>
+
+<?php
+
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ]
+];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Hotel</title>
+    <title>Filtered</title>
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 
 <body>
-    <?php
 
-    $hotels = [
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ]
-    ];
+    <?php
+    if ($filter = 'all') {
+        $hotels_filtered = $hotels;
+    } else if ($filter = 'yes') {
+        foreach ($hotels as $key => $value) {
+            if ($value['parking']) {
+                array_push($hotels_filtered, $value);
+            }
+        }
+    } else {
+        foreach ($hotels as $key => $value) {
+            if (!$value['parking']) {
+                array_push($hotels_filtered, $value);
+            }
+        }
+    }
     ?>
 
-    <div class="d-flex flex-column align-items-center mt-5">
-        <form action="filter.php" method="GET">
-            <button type="submit" class="btn btn-dark my-2">With Parking?</button>
-            <select class="form-select" aria-label="parking" id="filter" name="filter">
-                <option value="all">All</option>
-                <option value="yes">Yes</option>
-                <option value="none">No</option>
-            </select>
-        </form>
 
+    <div class="d-flex flex-column align-items-center mt-5">
         <div class="d-flex justify-content-center p-5">
             <table class="table">
                 <thead>
@@ -89,7 +104,6 @@
             </table>
         </div>
     </div>
-
 </body>
 
 </html>
